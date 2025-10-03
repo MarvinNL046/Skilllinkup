df8dc8613a61d3f5fcfb1c0a337d57b2af969329
MarvinNL046 <info@staycoolairco.nl>
2025-10-03 21:20:17 +0200

    feat: Add image optimization disable and CORS headers

--- next.config.js
+++ next.config.js
@@ -2,10 +2,26 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASEPATH : "",
-  // images: {
-  //   loader: 'akamai',
-  //   path: process.env.NEXT_PUBLIC_URL,
-  // },  
+
+  // Image optimization configuration
+  images: {
+    unoptimized: true, // Disable image optimization for Netlify
+  },
+
+  // CORS headers
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: 'https://skilllinkup.com' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE,OPTIONS,PATCH' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
